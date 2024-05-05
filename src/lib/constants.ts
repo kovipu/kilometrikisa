d99e4336e9c76ff9ce94b57e784a179c01edd534
Konsta Purtsi <konsta@arado.fi>
2024-05-05 11:26:32 +0300

    Update to track Kilometrikisa 2024

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,4 +1,4 @@
-const startDateTime = new Date('2023-05-01T00:00:00.000Z');
-const endDateTime = new Date('2023-09-30T23:59:59.999Z');
+const startDateTime = new Date('2024-05-01T00:00:00.000Z');
+const endDateTime = new Date('2024-09-30T23:59:59.999Z');
 
 export { startDateTime, endDateTime };
